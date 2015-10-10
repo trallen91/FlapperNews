9b55709b7e336d0b323905530693d8f2463fdfb7
trallen91 <travis.william.allen@gmail.com>
2015-10-10 16:09:06 -0400

    add new comments on comments page

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-angular.module("flapperNews", []).factory("posts", [function(){
+angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    }).state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}])
+
+angular.module("flapperNews").factory("posts", [function(){
   var o = {
     posts: []
   };
@@ -20,14 +40,34 @@ function($scope, posts){
 
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0,
+        comments: [
+          {author: "Joe", body: "Cool post!", upvotes: 0},
+          {author: "Bob", body: "Great idae but everything is wrong", upvotes: 0}
+        ]});
       $scope.title = '';
       $scope.link = '';
-      console.log($scope.posts)
     };
 
     $scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
     };
 }]);
+
+angular.module("flapperNews").controller("PostsCtrl", [
+  "$scope",
+  "$stateParams",
+  "posts",
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+      $scope.addComment = function(){
+        if($scope.body === ''){return;}
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body = '';
+      }
+  }])
 
