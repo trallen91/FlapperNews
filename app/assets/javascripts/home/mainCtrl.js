335c8888454365d3fe91d6ac300a7f73ec59192f
Travis Allen <travis.william.allen@gmail.com>
2015-10-13 10:13:53 -0400

    refator controllers/factories out of app.js

---
+++ app/assets/javascripts/home/mainCtrl.js
@@ -0,0 +1,21 @@
+angular.module('flapperNews').controller('MainCtrl', [
+  '$scope',
+  'posts',
+  function($scope, posts){
+    $scope.test = "Hello world";
+    $scope.posts = posts.posts;
+
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') { return; }
+    $scope.posts.push({title: $scope.title, link: $scope.link,upvotes: 0,
+      comments: [
+         {author: "jabroni", body: "Dum", upvotes: 5}
+        ]})
+    $scope.title = '';
+    $scope.link = '';
+  };
+
+  $scope.incrementUpvotes = function(post){
+    post.upvotes += 1;
+  }
+}]);
