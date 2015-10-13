3ad3209bed4df26f4f9770e1f9dc9d3a64ebe0ef
Travis Allen <travis.william.allen@gmail.com>
2015-10-13 17:37:35 -0400

    add code (from http://stackoverflow.com/questions/14734243/rails-csrf-protection-angular-js-protect-from-forgery-makes-me-to-log-out-on) to allow user log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,21 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  after_filter :set_csrf_cookie_for_ng
+
+def set_csrf_cookie_for_ng
+  cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+end
+
   def angular
     render 'layouts/application'
+  end
+
+  protected
+
+  # In Rails 4.2 and above
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
   end
 
   private
