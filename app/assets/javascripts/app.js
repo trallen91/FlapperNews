074dc6858b003b0767856d97a3397ec46d9b9c28
Travis Allen <travis.william.allen@gmail.com>
2015-10-13 17:33:14 -0400

    registration works, but logging in does not

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -28,6 +28,28 @@ angular.module('flapperNews').config([
         }
       });
 
+      $stateProvider.state('login', {
+        url: '/login',
+        templateUrl: 'auth/_login.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+      });
+
+      $stateProvider.state('register', {
+        url: '/register',
+        templateUrl: 'auth/_register.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+        }]
+      })
+
       $urlRouterProvider.otherwise('home');
   }])
 
